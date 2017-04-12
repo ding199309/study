fbb8952bc4cc0f142022b32c0e4f727d554a4984
ding199309 <ding199309@163.com>
2017-04-12 15:01:59 +0800

    第一步加pom.xml依赖

---
+++ src/main/java/com/spring/springboot/config/MasterConfig.java
@@ -0,0 +1,28 @@
+/** 
+ * Project Name:springboot-learn-demo1 
+ * File Name:MainConfig.java 
+ * Package Name:com.spring.springboot.config 
+ * Date:2017年4月12日下午2:06:00 
+ * Copyright (c) 2017, chenzhou1025@126.com All Rights Reserved. 
+ * 
+*/  
+  
+package com.spring.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/** 
+ * ClassName:MainConfig <br/> 
+ * Function: TODO 主数据源控制 <br/> 
+ * Date:     2017年4月12日 下午2:06:00 <br/> 
+ * @author   dfz
+ * @version   
+ * @since    JDK 1.8 
+ * @see       
+ */
+
+@Configuration
+public class MasterConfig {
+
+}
+  
---
+++ src/main/java/com/spring/springboot/config/SlaveConfig.java
@@ -0,0 +1,23 @@
+/** 
+ * Project Name:springboot-learn-demo1 
+ * File Name:SlaveConfig.java 
+ * Package Name:com.spring.springboot.config 
+ * Date:2017年4月12日下午2:09:19 
+ * Copyright (c) 2017, chenzhou1025@126.com All Rights Reserved. 
+ * 
+*/  
+  
+package com.spring.springboot.config;  
+/** 
+ * ClassName:SlaveConfig <br/> 
+ * Function: TODO 从配置源 <br/> 
+ * Date:     2017年4月12日 下午2:09:19 <br/> 
+ * @author   dfz
+ * @version   
+ * @since    JDK 1.8 
+ * @see       
+ */
+public class SlaveConfig {
+
+}
+  
