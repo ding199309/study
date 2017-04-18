628ea3e64d4e1789859d66051cef0391fe061a25
ding199309 <ding199309@163.com>
2017-04-18 15:59:02 +0800

    update pom

--- src/main/java/com/spring/springboot/RootController.java
+++ src/main/java/com/spring/springboot/RootController.java
@@ -3,15 +3,39 @@ package com.spring.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 
+
+/**
+ * 
+ * 文件名称为：RootController.java
+ * @author dfz
+ * @version  1.0.0
+ * @time  2017年4月18日 下午3:38:10
+ * @copyright Anymusic Ltd.
+ */
+
+//  继承SpringBootServletInitializer   主要是用于发布到tomcat中
 @SpringBootApplication
-public class RootController {
+public class RootController  extends SpringBootServletInitializer {
 
 
+	/*
+	 * 使用@SpringBootApplication注解。但是如果war包发布至tomcat，需要增加 SpringBootServletInitializer 子类，
+	 * 并覆盖它的 configure 方法，或者直接将main函数所在的类继承 SpringBootServletInitializer 子类，并覆盖它的 configure 方法。
+	 */
+	@Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder application) {
+        return application.sources(RootController.class);
+    }
+	
+	
+	
     public static void main(String[] args) throws Exception {
         // 程序启动入口
-        // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
         SpringApplication.run(RootController.class, args);
     }
 }
